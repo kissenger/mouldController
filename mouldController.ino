667e975b457f4ab1a5e0202306039f24aa7033c8
kissenger <ivyterrace@hotmail.co.uk>
2022-07-12 22:06:47 +0100

    update to include pressure sensor

--- mouldController.ino
+++ mouldController.ino
@@ -1,8 +1,15 @@
+
+/*
+ * With multiple sensor types it's less easy to avoid duplication of code, wo this approach is quick and dirty but it works
+ */
+
 #include <WiFiNINA.h>
 #include <Adafruit_AHTX0.h>
 #include <ArduinoJson.h>
 #include <Wire.h>
+#include <BMP180I2C.h>
 
+#define BMPADDR 0x77
 #define TCAADDR 0x70
 WiFiClient client;
 
@@ -10,60 +17,48 @@ WiFiClient client;
 char ssid[] = "IvyTerrace_EXT";
 char server[] = "www.thingummy.cc";
 int port = 80;
-int readInterval = 10;     // minutes
+int readInterval = 15;     // minutes
 bool isDeployed = true;
 
 // test config
-/*
-char ssid[] = "IvyTerrace";
-const IPAddress server(192,168,1,64);
-int port = 3000;
-int readInterval = 0.1;     // minutes
-bool isDeployed = false;
-*/
+//char ssid[] = "IvyTerrace";
+//const IPAddress server(192,168,1,64);
+//int port = 3000;
+//int readInterval = 0.1;     // minutes
+//bool isDeployed = false;
 
 char pass[] = "qwertyuiopisthetoprowofkeysonakeyboard";
 unsigned long lastBlinkTime = 0;    //will store last time Wi-Fi information was updated
 unsigned long lastSensorTime = 0;
+int nSamples = 50;    //average over this number of samples
+int sampleDelay = 10;   //ms delay between samples
 bool isFirstLoop = true;
 const unsigned long readSensorInterval = readInterval * 60UL * 1000UL;
-StaticJsonDocument<128> doc;
-char json[128];
-
-struct sensor {
-  char* sensorName;
-  char* sensorType;
-  bool isFound;
-  Adafruit_AHTX0 sensorObject;
-};
-
-sensor sensors[2];
+StaticJsonDocument<512> doc;
+char json[512];
+BMP180I2C bmpOutside(0x77);
+Adafruit_AHTX0 ahtInside;
+Adafruit_AHTX0 ahtOutside;
+sensors_event_t ahtHumidity, ahtTemp;
 
 void setup() {
   
   Serial.begin(9600);
-  delay(2000);
+  delay(500);
   Serial.println("Setup...");
   
   Wire.begin();
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
-  
-  sensors[0].sensorName = "ahtInside";
-  sensors[0].sensorType = "ahtx0"; 
-  sensors[0].isFound = false;
-  
-  sensors[1].sensorName = "ahtOutside";
-  sensors[1].sensorType = "ahtx0"; 
-  sensors[1].isFound = false;
 
-  //non-blocking check for aht
-  for (int i = 0; i < 2; i++) {
-    selectSensor(i);
-    if (sensors[i].sensorObject.begin()) {
-      sensors[i].isFound = true;
-    }
-  }
+  // Sensor 1
+  selectMuxChannel(0);
+  ahtInside.begin();
+
+  // Sensors 2 & 3
+  selectMuxChannel(1);
+  ahtOutside.begin();
+  bmpOutside.begin();
 
 }
 
@@ -87,40 +82,111 @@ void loop() {
 
     lastSensorTime = millis();
     isFirstLoop = false;
+    JsonObject root = doc.to<JsonObject>();
+    JsonArray arr = root.createNestedArray("data");
 
-    for (int i = 0; i < 2; i++) {
+    // Sensor 1
+    JsonObject sensor1_data = arr.createNestedObject();
+    selectMuxChannel(0);
+    sensor1_data["sensor_name"] = "ahtInside";
+    sensor1_data["sensor_type"] = "ahtx0";
+    sensor1_data["time"] = WiFi.getTime();   
+    sensor1_data["sensor_found"] = false; 
+    sensor1_data["deployed"] = isDeployed;     
+    if (ahtInside.getStatus() != 255) {
+      float tempSum = 0;
+      float rhSum = 0;
+      for (int i = 0; i < nSamples; i++) {
+        ahtInside.getEvent(&ahtHumidity, &ahtTemp);
+        tempSum += ahtTemp.temperature;
+        rhSum += ahtHumidity.temperature;
+        delay(sampleDelay);      
+      };     
+      sensor1_data["sensor_found"] = true;
+      sensor1_data["temp"] = tempSum / nSamples;
+      sensor1_data["rh"] = rhSum / nSamples;   
+    };
 
+    // Sensor 2
+    JsonObject sensor2_data = arr.createNestedObject();
+    selectMuxChannel(1);
+    sensor2_data["sensor_name"] = "ahtOutside";
+    sensor2_data["sensor_type"] = "ahtx0";
+    sensor2_data["time"] = WiFi.getTime();   
+    sensor2_data["sensor_found"] = false; 
+    sensor2_data["deployed"] = isDeployed;     
+    if (ahtOutside.getStatus() != 255) {
+      float tempSum = 0;
+      float rhSum = 0;
+      for (int i = 0; i < nSamples; i++) {
+        ahtOutside.getEvent(&ahtHumidity, &ahtTemp);
+        tempSum += ahtTemp.temperature;
+        rhSum += ahtHumidity.temperature;
+        delay(sampleDelay);      
+      };     
+      sensor2_data["sensor_found"] = true;
+      sensor2_data["temp"] = tempSum / nSamples;
+      sensor2_data["rh"] = rhSum / nSamples;  
+    };
+    
+    // Sensor 3
+    JsonObject sensor3_data = arr.createNestedObject();
+    selectMuxChannel(1);
+    ahtOutside.getEvent(&ahtHumidity, &ahtTemp);
+    sensor3_data["sensor_name"] = "bmpOutside";
+    sensor3_data["sensor_type"] = "bmp180";
+    sensor3_data["time"] = WiFi.getTime();  
+    sensor3_data["sensor_found"] = false;
+    sensor3_data["deployed"] = isDeployed;
+     
+    if (bmpStatus()) {
+      
+      float pressSum = 0;
+      float tempSum = 0;
+      
+      for (int i = 0; i < nSamples; i++) {
+        bmpOutside.measureTemperature(); 
+        while (!bmpOutside.hasValue() ) {
+          delay(100);
+        }; 
+        tempSum += bmpOutside.getTemperature();
+
+        bmpOutside.measurePressure(); 
+        while (!bmpOutside.hasValue()) delay(100);
+        pressSum += bmpOutside.getPressure();
+
+        sensor3_data["sensor_found"] = true;
+        sensor3_data["temp"] = tempSum / nSamples;
+        sensor3_data["press"] = pressSum / nSamples; 
+      } 
+
+    } 
         
-        selectSensor(i);
-        sensors_event_t ahtHumidity, ahtTemp;
-        sensors[i].sensorObject.getEvent(&ahtHumidity, &ahtTemp);
-        
-        doc["sensor_name"] = sensors[i].sensorName;
-        doc["sensor_type"] = sensors[i].sensorType;
-        doc["sensor_found"] = sensors[i].isFound;      
-        doc["time"] = WiFi.getTime();
-        doc["temp"] = sensors[i].isFound ? ahtTemp.temperature : 0.0;
-        doc["rh"] = sensors[i].isFound ? ahtHumidity.relative_humidity : 0.0;
-        serializeJson(doc, json);
-  
-        Serial.println(json);
-        
-        // send to server
-        if (client.connect(server, port)) {
-          String requestBody = json;
-          postData(requestBody);
-          Serial.println("request sent");
-        } else {
-          Serial.println("request not sent");
-        }
-
-        delay(500);
-
+    serializeJson(root, json);
+    Serial.println(json);
+    
+    // send to server
+    if (client.connect(server, port)) {
+      String requestBody = json;
+      postData(requestBody);
+      Serial.println("request sent");
+    } else {
+      Serial.println("request not sent");
     }
+
+    delay(500);
+
       
   }
 }
 
+bool bmpStatus(){
+  if(bmpOutside.measureTemperature()) {
+    delay(1000);
+    return true;
+  } 
+  return false;
+}
 
 void postData(String body) {
   // send HTTP request header
@@ -138,23 +204,21 @@ void postData(String body) {
   client.println(body);
 }
 
-
 void connectToWifi() {
-  
-    // attempt to connect to Wi-Fi network:
   while (WiFi.status() != WL_CONNECTED) {
     digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
     delay(100);
     digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));    
     Serial.print("Attempting connection to ");
-    Serial.println(ssid);
+    Serial.print(ssid);
+    Serial.println("...");
     WiFi.begin(ssid, pass);
     delay(10000);
   }
-  Serial.println("Connected");
+  Serial.println("Connected!");
 }
 
-void selectSensor(int i) {
+void selectMuxChannel(int i) {
   if (i > 7) return;
   Wire.beginTransmission(TCAADDR);
   Wire.write(1 << i);
